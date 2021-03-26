10fc289f166d14d9512f376a691f63a386223596
jda_10 <jatchley905@gmail.com>
2021-03-25 23:39:27 -0500

    WIP: Implemnent pen draw in app

--- src/App.js
+++ src/App.js
@@ -112,6 +112,11 @@ function App() {
       <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
     </svg>
     </Button>
+    <Button style={{margin: "5px"}} data-toggle="tooltip" title="Pen">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+      <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
+    </svg>
+    </Button>
     </div>
     <canvas 
     id="canvas" 
