d958ba93a902063081a9b6a8f9a57553e58407d9
jda_10 <jatchley905@gmail.com>
2021-03-26 16:10:12 -0500

    Disable undo and redo when there are no more elements. Updated demo.gif

--- src/App.js
+++ src/App.js
@@ -47,6 +47,19 @@ function App() {
     elements.forEach(({roughElement}) => {
       roughCanvas.draw(roughElement)
     });
+
+    if(elements.length < 1){
+      document.querySelector('.undo').disabled = true;
+    }
+    else{
+      document.querySelector('.undo').disabled = false;
+    }
+    if(discardedElements.length < 1){
+      document.querySelector('.redo').disabled = true;
+    }
+    else{
+      document.querySelector('.redo').disabled = false;
+    }
   }
   const handleMouseMove = (event) => {
     if(!drawing){return}
@@ -85,13 +98,14 @@ function App() {
   }
 
   const clear = () => {
+    discardedElements.splice(0,discardedElements.length) 
     elements.splice(0,elements.length) 
     setElements(elements);
     redraw();
   }
   return (
     <div>
-    <div style={{position:"fixed", display:"flex", left: "25%", transform : "translate(-25%, 0)"}}>
+    <div style={{position:"fixed", display:"flex", left: "25%", transform : "translate(-25%, 0)", backgroundColor: "lightgray"}} className="toolbar">
     <Button style={{margin: "5px"}}
     onClick={() => setElementType("rectangle")}
     data-toggle="tooltip" title="Create rectangle"
@@ -110,25 +124,25 @@ function App() {
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   </svg>
     </Button>
-    <Button style={{margin: "5px"}} onClick={() => undo()} data-toggle="tooltip" title="Undo">
+    <Button style={{margin: "5px"}} onClick={() => undo()} data-toggle="tooltip" title="Undo" className="undo">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
       <path fillRule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
       <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
     </svg>
     </Button>
-    <Button style={{margin: "5px"}} onClick={() => redo()} data-toggle="tooltip" title="Redo">
+    <Button style={{margin: "5px"}} onClick={() => redo()} data-toggle="tooltip" title="Redo" className="redo">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
       <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
       <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
     </svg>
     </Button>
-    <Button style={{margin: "5px"}} data-toggle="tooltip" title="Pen" onClick={()=>alert("Currently in progress")}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+    <Button style={{margin: "5px"}} data-toggle="tooltip" title="Pen" onClick={()=>alert("Currently in progress")} disabled>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen-fill" viewBox="0 0 16 16">
       <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
     </svg>
     </Button>
     <Button style={{margin: "5px"}} data-toggle="tooltip" title="Clear" onClick={()=>{clear()}}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-square-fill" viewBox="0 0 16 16">
       <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
     </svg>
     </Button>
