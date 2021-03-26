2ebd63d9fdeefd35dc8387333dce18421aa22e64
jda_10 <jatchley905@gmail.com>
2021-03-25 23:13:52 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLayoutEffect, useState } from "react";
+import rough from 'roughjs/bundled/rough.esm';
+import Button from 'react-bootstrap/Button'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+const generator = rough.generator();
+
+function createElement(x1,y1,x2,y2, type){
+
+  let roughElement = null;
+  if(type === "line"){roughElement= generator.line(x1,y1, x2, y2)}
+  else if(type === "rectangle"){roughElement = generator.rectangle(x1,y1, x2-x1, y2-y1)}
+  else{roughElement = generator.ellipse(x1,y1, x2-x1, y2-y1)}
+
+  return{x1,y1,x2,y2, roughElement};
+}
 
 function App() {
+
+
+  const[elements, setElements] = useState([]);
+  const [drawing, setDrawing] = useState(false);
+  const [elementType, setElementType] = useState("line");
+  const [discardedElements] = useState([]);
+  useLayoutEffect(() => {
+    redraw();
+  })
+
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+    const {clientX, clientY} = event;
+
+    const element = createElement(clientX, clientY,clientX, clientY, elementType);
+    setElements(prevState => [...prevState, element]);
+  };
+  const redraw = () => {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    context.clearRect(0,0, canvas.width, canvas.height);
+
+    const roughCanvas = rough.canvas(canvas);
+
+    elements.forEach(({roughElement}) => {
+      roughCanvas.draw(roughElement)
+    });
+  }
+  const handleMouseMove = (event) => {
+    if(!drawing){return}
+    else{
+      const {clientX, clientY} = event;
+      const index = elements.length -1;
+      const {x1, y1} = elements[index];
+      const updatedElement= createElement(x1 , y1, clientX, clientY, elementType);
+
+      const elementsCopy = [...elements];
+      elementsCopy[index] = updatedElement;
+      setElements(elementsCopy);
+    }
+  };
+ 
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
+
+  const undo = () => {
+    //Don't pop from array if there are no more elements to pop
+    if(elements.length > 0){
+      discardedElements.push(elements.pop())
+      setElements(elements);
+      redraw();
+    }
+  }
+
+  const redo = () => {
+    if(discardedElements.length > 0){
+      elements.push(discardedElements.pop())
+      setElements(elements);
+      redraw();
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div style={{position:"fixed", display:"flex", left: "25%", transform : "translate(-25%, 0)"}}>
+    <Button style={{margin: "5px"}}
+    onClick={() => setElementType("rectangle")}
+    
+    >
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file" viewBox="0 0 16 16">
+      <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+    </svg>
+    </Button>
+    <Button style={{margin: "5px"}} onClick={() => setElementType("line")}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-right" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+    </svg>
+    </Button>
+    <Button style={{margin: "5px"}} onClick={() => setElementType("circle")}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  </svg>
+    </Button>
+    <Button style={{margin: "5px"}} onClick={() => undo()}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+      <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+    </svg>
+    </Button>
+    <Button style={{margin: "5px"}} onClick={() => redo()}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+    </svg>
+    </Button>
+    </div>
+    <canvas 
+    id="canvas" 
+    width={window.innerWidth} 
+    height={window.innerHeight}
+    onMouseDown={handleMouseDown}
+    onMouseMove={handleMouseMove}
+    onMouseUp={handleMouseUp}
+    ></canvas>
     </div>
   );
 }
